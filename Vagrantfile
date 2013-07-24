d9267f862d71bb65d8db50124319a9f18d50d054
Sveinung Dalatun <sdalatun@gmail.com>
2013-07-24 21:40:31 +0200

    Install node in the VM

--- Vagrantfile
+++ Vagrantfile
@@ -4,4 +4,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.provision :shell, :path => "vagrantsetup.sh"
 end
---
+++ vagrantsetup.sh
@@ -0,0 +1,9 @@
+# Dependencies for the test environment
+
+sudo apt-get update
+
+sudo apt-get -q -y install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -q -y install nodejs
+
